c8ce22f2dff25c9bb0909b12a6eb581e7fc40039
sherri.ice <sherri.ice7@gmail.com>
2021-10-27 21:55:21 +0300

    fixed vk permissions

--- vk_auth.py
+++ vk_auth.py
@@ -9,7 +9,7 @@ from loader import VK_API_APP_ID, REDIRECT_FROM_VK, VK_CLIENT_SECRET
 def request_vk_auth_code(id: int) -> str:
     vk_request_auth_url = "https://oauth.vk.com/authorize?" \
                           f"client_id={VK_API_APP_ID}&" \
-                          "scope=friends&" \
+                          "scope=friends,status&" \
                           f"redirect_uri={REDIRECT_FROM_VK}?tg_id={id}&" \
                           "response_type=code&" \
                           "v=5.131"
@@ -24,3 +24,10 @@ def authorize_vk_session(code: str, id: int):
         print(error_msg)
         return None
     return vk_session.token
+
+
+# if __name__ == '__main__':
+#     print(request_vk_auth_code(123))
+#     vk = vk_api.VkApi(token = "fc33ca223cebea13b5a**26a3813aca733aaeac0b72eb886bcc681c79e4617552d9ce029d60409572aed6")
+#     vk.get_api()
+#     print(vk.get_api().account.getProfileInfo())
