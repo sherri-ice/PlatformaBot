80de5e6cde0fdfd7199d3bfb979fdcbbb6aa169a
sherri.ice <sherri.ice7@gmail.com>
2021-10-27 11:03:30 +0300

    [test] fixed vk_auth redirect url

--- vk_auth.py
+++ vk_auth.py
@@ -9,6 +9,10 @@ def request_vk_auth(id: int) -> str:
                           f"client_id={VK_API_APP_ID}&" \
                           "scope=friends&" \
                           f"redirect_uri={REDIRECT_FROM_VK}?tg_id={id}&" \
-                          "response_type=token, id&" \
+                          "response_type=token&" \
                           "v=5.131"
     return vk_request_auth_url
+
+
+if __name__ == '__main__':
+    print(request_vk_auth(299065458))
