fe824279a9092e1e7a6ac8d5acf51f4c3d93eb93
sherri.ice <sherri.ice7@gmail.com>
2021-10-29 23:03:59 +0300

    re registration

--- telegram_bot.py
+++ telegram_bot.py
@@ -65,21 +65,17 @@ def register(message):
         tg_bot.send_message(message.chat.id, messages_templates["unregistered_user"]["registration_start"])
         tg_bot.register_next_step_handler(message, process_name_step)
     else:
-        tg_bot.register_next_step_handler(message, re_register)
-
-
-def re_register(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.row_width = 4
-    markup.add(types.InlineKeyboardButton("Да!", callback_data = "cd_yes"),
-               types.InlineKeyboardButton("Оставить всё как есть", callback_data = "cd_no"),
-               )
-    tg_bot.send_message(message.chat.id, "Ты уже авторизирован. Уверен, что хочешь перерегистрировать профиль?",
-                        reply_markup = markup)
+        markup = types.InlineKeyboardMarkup()
+        markup.row_width = 2
+        markup.add(types.InlineKeyboardButton("Да!", callback_data = "cd_yes"),
+                   types.InlineKeyboardButton("Оставить всё как есть", callback_data = "cd_no"),
+                   )
+        tg_bot.send_message(message.chat.id, "Ты уже авторизирован. Уверен, что хочешь перерегистрировать профиль?",
+                            reply_markup = markup)
 
 
 # Handles answer for inline keyboard
-@tg_bot.callback_query_handler(func=lambda call: True)
+@tg_bot.callback_query_handler(func = lambda call: True)
 def handle_callback(call):
     if call.data == "cd_yes":
         tg_bot.answer_callback_query(call.id, "Да")
