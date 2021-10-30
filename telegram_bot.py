4ee5390198f599df6f832fbd8c3c8941a3615ab3
sherri.ice <sherri.ice7@gmail.com>
2021-10-30 14:41:58 +0300

    fix vk_re_auth_call

--- telegram_bot.py
+++ telegram_bot.py
@@ -58,10 +58,11 @@ def cancel_vk_auth(call):
     tg_bot.send_message(call.message.chat.id, messages_templates["vk"]["vk_cancel_auth"])
 
 
-@tg_bot.callback_query_handler(func = lambda call: call.data == "cd_vk_reauth_cancel")
+@tg_bot.callback_query_handler(func = lambda call: call.data == "cd_vk_reauth")
 def cancel_vk_auth(call):
+    tg_bot.answer_callback_query(call.id, "Выбрать другой аккаунт")
     tg_bot.send_message(call.message.chat.id, messages_templates["vk"]["vk_auth_message"],
-                        reply_markup = gen_markup_for_vk_auth())
+                        reply_markup = gen_markup_for_vk_auth(call.message.chat.id))
 
 
 def after_vk_auth_in_server(tg_id):
