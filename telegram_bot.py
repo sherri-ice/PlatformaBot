6157e2ab374e6214a74f9675c83dc4fb3ad258d3
sherri.ice <sherri.ice7@gmail.com>
2021-11-02 17:11:59 +0300

    [debug]

--- telegram_bot.py
+++ telegram_bot.py
@@ -147,7 +147,7 @@ def handle_callback_faq(call):
 @tg_bot.callback_query_handler(func = lambda call: call.data == "cd_user_ready")
 def user_ready(call):
     tg_bot.delete_message(chat_id = call.message.chat.id, message_id = call.message.message_id)
-    tg_bot.send_message(call.from_user.id, messages_templates["is_user_ready"])
+    command_choose_role(call.message)
 
 
 @tg_bot.message_handler(commands = ['choose_role'])
