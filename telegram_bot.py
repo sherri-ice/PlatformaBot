7abf4a5963bffc51d8ace4f7eb3fdc0ac2ce4547
sherri.ice <sherri.ice7@gmail.com>
2021-11-02 13:58:20 +0300

    fix message for choosing role

--- telegram_bot.py
+++ telegram_bot.py
@@ -146,9 +146,8 @@ def handle_callback_faq(call):
 @tg_bot.callback_query_handler(func = lambda call: call.data == "cd_user_ready")
 def user_ready(call):
     tg_bot.delete_message(chat_id = call.message.chat.id, message_id = call.message.message_id)
-    keyboard = {"Выбрать исполнителя": "cd_employee", "Выбрать заказчика": "cd_customer"}
     tg_bot.send_message(call.message.chat.id, messages_templates["choose_role"],
-                        reply_markup = create_inline_keyboard(keyboard))
+                        reply_markup = create_inline_keyboard(buttons["choose_type_of_account"]))
 
 
 def gen_markup_for_vk_auth(chat_id):
