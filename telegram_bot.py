306b7c9537a1f8f74702889e987dcd9d3569ec3e
sherri.ice <sherri.ice7@gmail.com>
2021-11-02 11:06:03 +0300

    fix for registration

--- telegram_bot.py
+++ telegram_bot.py
@@ -39,7 +39,7 @@ def callback_reg(call):
     tg_bot.answer_callback_query(call.id, "Рег")
     message = tg_bot.current_states._states if tg_bot.current_states._states is None else "empty"
     tg_bot.send_message(call.from_user.id, message)
-    tg_bot.set_state(call.from_user.id, "reg")
+    tg_bot.register_next_step_handler(call.message, command_register)
 
 
 @tg_bot.message_handler(commands = ['start'])
