005f563d0bc0ba013ba50b2a266fd021d1e7b45b
sherri.ice <sherri.ice7@gmail.com>
2021-11-03 14:40:11 +0300

    fix for vk empty token

--- telegram_bot.py
+++ telegram_bot.py
@@ -160,7 +160,8 @@ def get_employee_profile_info(user_id):
         keyboard.add(types.InlineKeyboardButton("VK auth", callback_data = "cd_vk_auth"))
     if employee.insta_access_token is None:
         keyboard.add(types.InlineKeyboardButton("Insta auth", callback_data = "cd_vk_auth"))
-    message = messages_templates["employee"]["profile"].format(get_vk_profile_info_for_employee(user_id),
+    message = messages_templates["employee"]["profile"].format("Не авторизирован" if employee.vk_access_token is None
+                                                               else get_vk_profile_info_for_employee(user_id),
                                                                "Не авторизирован" if employee.insta_access_token is
                                                                                      None else employee.insta_access_token,
                                                                employee.balance)
