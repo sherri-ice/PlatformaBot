0fc56df0fbd7e587d39a2451f5680c11022fb550
sherri.ice <sherri.ice7@gmail.com>
2021-11-18 09:34:43 +0300

    functions in task_table

--- sql/task/task.py
+++ sql/task/task.py
@@ -15,12 +15,36 @@ class Task(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
     creation_date = db.Column(db.Date, default = datetime.now())
     platform = db.Column(db.String(255))
+    # for target
+    age = db.Column(db.Integer)
+    city = db.Column(db.String)
+    salary = db.Column(db.String)
+    #
     employee = db.relationship("Employee", backref = db.backref("employee_id", uselist = False))
     customer = db.relationship("Customer", backref = db.backref("customer_id", uselist = False))
 
     def add_new_task(self, customer_id, ref, guarantee, platform):
         db.session.add(Task(ref = ref, customer_id = customer_id, guarantee = guarantee, platform = platform))
         apply_db_changes()
+
+    def add_new_target_task(self):
+        pass
+
+    def get_tasks_by_customer_id(self, customer_id):
+        return self.query.filter_by(customer_id = customer_id)
+
+    def get_tasks_by_employee_id(self, employee_id):
+        return self.query.filter_by(employee_id = employee_id)
+
+    def get_new_tasks(self):
+        return self.query.filter_by(free = True)
+
+    def set_employee_id_for_task(self, task_id, employee_id):
+        task = self.query.filter_by(id = task_id).first()
+        if task is None:
+            return
+        task.employee_id = employee_id
+        return
 
 
 task_table = Task()
