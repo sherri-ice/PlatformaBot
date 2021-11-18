45cc23cdbf48f4c7eb7862dd27ae6bb4852618ee
sherri.ice <sherri.ice7@gmail.com>
2021-11-18 11:51:40 +0300

    [debug]

--- tg_bot/telegram_bot.py
+++ tg_bot/telegram_bot.py
@@ -330,7 +330,7 @@ def get_employee_profile_info(user_id):
 
 @tg_bot.callback_query_handler(func = lambda call: call.data == "cd_employee_get_new_task")
 def employee_get_new_task(call):
-    tg_bot.answer_callback_query(call.from_user.id, text = "P")
+    tg_bot.send_message(call.message.chat.id, "Pressed task")
 
 
 def get_customer_profile_info(user_id):
