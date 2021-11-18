e6ea28c122eeddfdc150ba7887dceab3ec76ae84
sherri.ice <sherri.ice7@gmail.com>
2021-11-18 08:55:00 +0300

    task implementation

--- sql/task/task.py
+++ sql/task/task.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+
 from sql.database import db, apply_db_changes
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer, primary_key = True, )
+    id = db.Column(db.Integer, primary_key = True)
+    # check if url is valid
+    ref = db.Column(db.String(255))
+    completed = db.Column(db.Boolean, default = False)
+    guarantee = db.Column(db.Integer)
+    creation_date = db.Column(db.Date, default = datetime.now())
+    platform = db.Column(db.String(255))
